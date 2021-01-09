21adde26f361ed49bdcc1d2a674510903d3cf6a7
Juggernau9 <xbhavyatomar@gmail.com>
2021-01-10 01:58:20 +0530

    scaffolding goal components

---
+++ src/components/DisplayGoals.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function DisplayGoals() {
+  return <div>DisplayGoals</div>;
+}
+
+export default DisplayGoals;
---
+++ src/components/NewGoalForm.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function NewGoalForm() {
+  return <div>NewGoalForm</div>;
+}
+
+export default NewGoalForm;
