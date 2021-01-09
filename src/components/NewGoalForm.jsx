8f507f81e30701b3d947da3b91e312aecec76c3f
Juggernau9 <xbhavyatomar@gmail.com>
2021-01-10 03:14:35 +0530

    add functionality to create new goal

--- src/components/NewGoalForm.jsx
+++ src/components/NewGoalForm.jsx
@@ -1,7 +1,39 @@
-import React from "react";
+import useInputState from "../hooks/useInputState";
 
-function NewGoalForm() {
-  return <div>NewGoalForm</div>;
+function NewGoalForm({ setGoals }) {
+  const [name, setName, resetName] = useInputState("");
+  const [desc, setDesc, resetDesc] = useInputState("");
+  // time added assumed new Date() for now
+
+  const resetForm = () => {
+    resetName();
+    resetDesc();
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    const newGoal = { name, description: desc, timeAdded: new Date() };
+    setGoals((prevGoals) => {
+      return [...prevGoals, newGoal];
+    });
+    resetForm();
+  };
+
+  return (
+    <form onSubmit={handleFormSubmit}>
+      <label>Name</label>
+      <input value={name} onChange={setName} />
+      <br />
+      <label>Description</label>
+      <input value={desc} onChange={setDesc} />
+      <br />
+      <button type="submit">Create new goal</button>
+    </form>
+  );
 }
 
 export default NewGoalForm;
+
+// To Do - update Form with proper Goal structure
+// Validate Form
+// add uuid to goal
