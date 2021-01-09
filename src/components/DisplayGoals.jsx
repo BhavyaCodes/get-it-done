e16ed3728116388afcbeded3bd465958f32dabd1
Juggernau9 <xbhavyatomar@gmail.com>
2021-01-10 03:43:06 +0530

    display goals on DisplayGoals component

--- src/components/DisplayGoals.jsx
+++ src/components/DisplayGoals.jsx
@@ -1,8 +1,18 @@
 import React from "react";
 
 function DisplayGoals({ goals }) {
+  const renderGoals = () => {
+    return goals.map((goal) => (
+      <div key={goal._id}>
+        <h4>{goal.name}</h4>
+        <p>{goal.description}</p>
+        <p>{new Date(goal.timeAdded).toString()}</p>
+      </div>
+    ));
+  };
+
   console.log(goals);
-  return <div>DisplayGoals</div>;
+  return <div>{renderGoals()}</div>;
 }
 
 export default DisplayGoals;
