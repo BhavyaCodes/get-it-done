d71ce14028513a901f96ab0bfc09ef0ea92a340e
Juggernau9 <xbhavyatomar@gmail.com>
2021-01-11 02:12:49 +0530

    display hashTags in DisplayGoals component

--- src/components/DisplayGoals.jsx
+++ src/components/DisplayGoals.jsx
@@ -1,12 +1,23 @@
 import React from "react";
+import Chip from "@material-ui/core/Chip";
 
 function DisplayGoals({ goals }) {
+  const renderHashTags = (hashTags) => {
+    console.log(hashTags);
+    return hashTags.map((hashTag) => (
+      <Chip key={hashTag} label={hashTag} clickable color="secondary" />
+    ));
+  };
+
   const renderGoals = () => {
     return goals.map((goal) => (
       <div key={goal._id}>
         <h4>{goal.name}</h4>
         <p>{goal.description}</p>
         <p>{new Date(goal.timeAdded).toString()}</p>
+        {goal?.hashTags?.length &&
+          goal?.hashTags?.length !== 0 &&
+          renderHashTags(goal.hashTags)}
       </div>
     ));
   };
