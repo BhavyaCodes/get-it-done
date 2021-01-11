e5da927d375cf6c1c2b3551e2ffc7821312fdd32
Juggernau9 <xbhavyatomar@gmail.com>
2021-01-11 22:20:00 +0530

    added mui Dialog and delete goal feature

--- src/components/DisplayGoals.jsx
+++ src/components/DisplayGoals.jsx
@@ -1,16 +1,38 @@
 import React from "react";
+import Button from "@material-ui/core/Button";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
+import { useTheme } from "@material-ui/core/styles";
 import Chip from "@material-ui/core/Chip";
 
-function DisplayGoals({ goals }) {
+function DisplayGoals({ goals, setGoals }) {
+  const [open, setOpen] = React.useState(false);
+  const theme = useTheme();
+  const fullScreen = useMediaQuery(theme.breakpoints.down("sm"));
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   const renderHashTags = (hashTags) => {
-    console.log(hashTags);
     return hashTags.map((hashTag) => (
       <Chip key={hashTag} label={hashTag} clickable color="secondary" />
     ));
   };
 
   const handleDeleteGoal = (goalId) => {
-    console.log(goalId);
+    setGoals((prevGoals) => {
+      return prevGoals.filter((prevGoal) => prevGoal._id !== goalId);
+    });
+    handleClose();
   };
 
   const renderGoals = () => {
@@ -22,14 +44,36 @@ function DisplayGoals({ goals }) {
         {goal?.hashTags &&
           goal?.hashTags?.length !== 0 &&
           renderHashTags(goal.hashTags)}
-        <button
-          type="button"
-          onClick={() => {
-            handleDeleteGoal(goal._id);
-          }}
-        >
+        <button type="button" onClick={handleClickOpen}>
           Delete Goal
         </button>
+        <Dialog
+          fullScreen={fullScreen}
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="responsive-dialog-title"
+        >
+          <DialogTitle id="responsive-dialog-title">
+            {`Delete ${goal.name}?`}
+          </DialogTitle>
+          <DialogContent>
+            <DialogContentText>{goal.description}</DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button autoFocus onClick={handleClose} color="default">
+              No
+            </Button>
+            <Button
+              onClick={() => {
+                handleDeleteGoal(goal._id);
+              }}
+              color="secondary"
+              autoFocus
+            >
+              Yes
+            </Button>
+          </DialogActions>
+        </Dialog>
       </div>
     ));
   };
