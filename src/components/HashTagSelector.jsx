f4173a6eecfc425a75955ee6d8ad95e9b806a308
Juggernau9 <xbhavyatomar@gmail.com>
2021-01-13 00:14:41 +0530

    update styling of buttons

--- src/components/HashTagSelector.jsx
+++ src/components/HashTagSelector.jsx
@@ -16,14 +16,11 @@ import {
 
 function HashTagSelector({ hashTags, setHashTags, globalHashTags }) {
   const handleAddHashTag = (hashTag) => {
-    alert("adding");
-    console.log(hashTag);
     setHashTags((prevHashTags) => {
       return { ...prevHashTags, [hashTag._id]: hashTag };
     });
   };
   const handleRemoveHashTag = (hashTag) => {
-    alert("removing");
     setHashTags((prevHashTags) => {
       delete prevHashTags[hashTag._id];
       return { ...prevHashTags };
@@ -42,43 +39,64 @@ function HashTagSelector({ hashTags, setHashTags, globalHashTags }) {
     grey,
   };
 
-  const ColorButton = withStyles((theme) => ({
-    root: {
-      color: theme.palette.getContrastText(colors.yellow[500]),
-      backgroundColor: colors.yellow[500],
-      "&:hover": {
-        backgroundColor: colors.yellow[700],
-      },
-    },
-  }))(Button);
-
   return (
     <div>
-      {globalHashTags.map((globalHashTag) =>
-        hashTags[globalHashTag._id] ? (
-          <ColorButton
-            size="small"
-            key={globalHashTag._id}
-            onClick={() => {
-              handleRemoveHashTag(globalHashTag);
-            }}
-            variant="outlined"
-          >
-            {globalHashTag.tag}
-          </ColorButton>
-        ) : (
-          <ColorButton
-            size="small"
-            key={globalHashTag._id}
-            onClick={() => {
-              handleAddHashTag(globalHashTag);
-            }}
-            variant="outlined"
-          >
-            {globalHashTag.tag}
-          </ColorButton>
-        )
-      )}
+      {globalHashTags.map((globalHashTag) => {
+        if (hashTags[globalHashTag._id]) {
+          const ColorButton = withStyles((theme) => ({
+            root: {
+              color: theme.palette.getContrastText(
+                colors[globalHashTag.color][500]
+              ),
+              backgroundColor: colors[globalHashTag.color][500],
+              "&:hover": {
+                backgroundColor: colors[globalHashTag.color][700],
+              },
+            },
+          }))(Button);
+
+          return (
+            <ColorButton
+              size="small"
+              key={globalHashTag._id}
+              onClick={() => {
+                handleRemoveHashTag(globalHashTag);
+              }}
+              variant="outlined"
+            >
+              {globalHashTag.tag}
+            </ColorButton>
+          );
+        } else {
+          const ColorButton = withStyles((theme) => ({
+            root: {
+              color: colors[globalHashTag.color][500],
+              backgroundColor: "colors[globalHashTag.color][500]",
+              borderColor: colors[globalHashTag.color][500],
+              "&:hover": {
+                backgroundColor: colors[globalHashTag.color][700],
+                color: theme.palette.getContrastText(
+                  colors[globalHashTag.color][500]
+                ),
+                borderColor: colors[globalHashTag.color][700],
+              },
+            },
+          }))(Button);
+
+          return (
+            <ColorButton
+              size="small"
+              key={globalHashTag._id}
+              onClick={() => {
+                handleAddHashTag(globalHashTag);
+              }}
+              variant="outlined"
+            >
+              {globalHashTag.tag}
+            </ColorButton>
+          );
+        }
+      })}
     </div>
   );
 }
