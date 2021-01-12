655289fd153e17246ade0c6b64b45019e6058638
Juggernau9 <xbhavyatomar@gmail.com>
2021-01-12 22:30:25 +0530

    add button to HashTagSelector component

--- src/components/HashTagSelector.jsx
+++ src/components/HashTagSelector.jsx
@@ -1,4 +1,18 @@
 import React from "react";
+
+import { withStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import {
+  red,
+  pink,
+  green,
+  purple,
+  blue,
+  teal,
+  lime,
+  yellow,
+  grey,
+} from "@material-ui/core/colors";
 import Chip from "@material-ui/core/Chip";
 
 function HashTagSelector({ hashTags, setHashTags, globalHashTags }) {
@@ -11,6 +25,28 @@ function HashTagSelector({ hashTags, setHashTags, globalHashTags }) {
       return prevHashTags.filter((prevHashTag) => hashTag !== prevHashTag);
     });
   };
+
+  const colors = {
+    red,
+    pink,
+    green,
+    purple,
+    blue,
+    teal,
+    lime,
+    yellow,
+    grey,
+  };
+
+  const ColorButton = withStyles((theme) => ({
+    root: {
+      color: theme.palette.getContrastText(colors.yellow[500]),
+      backgroundColor: colors.yellow[500],
+      "&:hover": {
+        backgroundColor: colors.yellow[700],
+      },
+    },
+  }))(Button);
 
   return (
     <div>
@@ -38,6 +74,7 @@ function HashTagSelector({ hashTags, setHashTags, globalHashTags }) {
           />
         )
       )}
+      <ColorButton>Health</ColorButton>
     </div>
   );
 }
