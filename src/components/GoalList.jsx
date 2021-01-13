8f2b97e569944eb6bb7cb36c72e6af5ef1f1d6a9
Juggernau9 <xbhavyatomar@gmail.com>
2021-01-14 00:00:35 +0530

    add state to GoalList form

--- src/components/GoalList.jsx
+++ src/components/GoalList.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Goal from "./Goal";
+import useInputState from "../hooks/useInputState";
 
 function GoalList({ goals, setGoals, globalHashTags }) {
+  const [searchText, setSearchText, resetSearchText] = useInputState("");
+  const [selectedHastagIds, setSelectedHashTagIds] = useState([]);
+  const [filteredByTextGoals, setFilteredByTextGoals] = useState(goals);
+
+  console.log(filteredByTextGoals);
+
+  useEffect(() => {
+    console.log("useEffect");
+    if (!searchText) {
+      return setFilteredByTextGoals(goals);
+    }
+  }, [searchText, goals]);
+
   const renderGoals = () => {
     return goals.map((goal) => (
       <Goal
@@ -13,7 +27,13 @@ function GoalList({ goals, setGoals, globalHashTags }) {
     ));
   };
 
-  return <div>{renderGoals()}</div>;
+  return (
+    <div>
+      <label>Filter text</label>
+      <input value={searchText} onChange={setSearchText} />
+      {renderGoals()}
+    </div>
+  );
 }
 
 export default GoalList;
