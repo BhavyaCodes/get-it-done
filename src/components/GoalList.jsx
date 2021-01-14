12f4ffd9b349783e979fe4368691bff70ce3550b
Juggernaut9 <58269749+Juggernaut9@users.noreply.github.com>
2021-01-14 15:29:32 +0530

    Sorting goals (#9)
    * add sorting menu
    
    * fix bug of filter not working
    
    * sort by duration prop
    
    * finish sorting

--- src/components/GoalList.jsx
+++ src/components/GoalList.jsx
@@ -2,12 +2,38 @@ import React, { useState, useEffect } from "react";
 import Goal from "./Goal";
 import useInputState from "../hooks/useInputState";
 
+import SortIcon from "@material-ui/icons/Sort";
+import MenuList from "@material-ui/core/MenuList";
+import ArrowUpwardIcon from "@material-ui/icons/ArrowUpward";
+import ArrowDownwardIcon from "@material-ui/icons/ArrowDownward";
+import IconButton from "@material-ui/core/IconButton";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import Typography from "@material-ui/core/Typography";
+
 import SelectTagFilters from "./SelectTagFilters";
 
 function GoalList({ goals, setGoals, globalHashTags }) {
   const [searchText, setSearchText, resetSearchText] = useInputState("");
   const [selectedHastagIds, setSelectedHashTagIds] = useState({});
   const [filteredGoals, setFilteredGoals] = useState(goals);
+  const [sortedGoals, setSortedGoals] = useState(filteredGoals);
+
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  useEffect(() => {
+    setSortedGoals(filteredGoals);
+  }, [filteredGoals]);
 
   useEffect(() => {
     const textFilter = !searchText
@@ -44,7 +70,8 @@ function GoalList({ goals, setGoals, globalHashTags }) {
   }, [searchText, goals, selectedHastagIds]);
 
   const renderGoals = () => {
-    return filteredGoals.map((goal) => (
+    console.log(sortedGoals);
+    return sortedGoals.map((goal) => (
       <Goal
         goal={goal}
         setGoals={setGoals}
@@ -53,6 +80,8 @@ function GoalList({ goals, setGoals, globalHashTags }) {
       />
     ));
   };
+
+  // console.log(sortedGoals);
 
   return (
     <div>
@@ -63,6 +92,80 @@ function GoalList({ goals, setGoals, globalHashTags }) {
         globalHashTags={globalHashTags}
       />
       <input value={searchText} onChange={setSearchText} />
+      <IconButton
+        aria-label="more"
+        aria-controls="long-menu"
+        aria-haspopup="true"
+        onClick={handleClick}
+      >
+        <SortIcon />
+      </IconButton>
+      <Menu
+        id="long-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={open}
+        onClose={handleClose}
+      >
+        <MenuList>
+          <MenuItem
+            onClick={() => {
+              setSortedGoals((prevGoals) => {
+                return prevGoals.sort(function (a, b) {
+                  const duration1 = a.isActive
+                    ? a.duration +
+                      (new Date().getTime() -
+                        new Date(a.latestStartTimeStamp).getTime()) /
+                        1000
+                    : a.duration;
+
+                  const duration2 = b.isActive
+                    ? b.duration +
+                      (new Date().getTime() -
+                        new Date(b.latestStartTimeStamp).getTime()) /
+                        1000
+                    : b.duration;
+                  return duration1 - duration2;
+                });
+              });
+              handleClose();
+            }}
+          >
+            <ListItemIcon>
+              <ArrowDownwardIcon fontSize="small" />
+            </ListItemIcon>
+            <Typography variant="inherit">Time spent</Typography>
+          </MenuItem>
+          <MenuItem
+            onClick={() => {
+              setSortedGoals((prevGoals) => {
+                return prevGoals.sort(function (a, b) {
+                  const duration1 = a.isActive
+                    ? a.duration +
+                      (new Date().getTime() -
+                        new Date(a.latestStartTimeStamp).getTime()) /
+                        1000
+                    : a.duration;
+
+                  const duration2 = b.isActive
+                    ? b.duration +
+                      (new Date().getTime() -
+                        new Date(b.latestStartTimeStamp).getTime()) /
+                        1000
+                    : b.duration;
+                  return duration2 - duration1;
+                });
+              });
+              handleClose();
+            }}
+          >
+            <ListItemIcon>
+              <ArrowUpwardIcon fontSize="small" />
+            </ListItemIcon>
+            <Typography variant="inherit">Time spent</Typography>
+          </MenuItem>
+        </MenuList>
+      </Menu>
       {renderGoals()}
     </div>
   );
